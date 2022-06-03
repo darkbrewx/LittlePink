af7c48e3c857cf035450889efa713b2f18f51f7c
HUWENBO <darkbrewx@gmail.com>
2022-06-03 16:07:26 +0800

    #add textview's lineSpacing and config

--- LittlePink/NoteEdit/NoteEditVC-Config.swift
+++ LittlePink/NoteEdit/NoteEditVC-Config.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 extension NoteEditVC{
     func  config(){
@@ -16,5 +17,18 @@ extension NoteEditVC{
         // remove textView padding
         let lineFramentPadding = noteTextView.textContainer.lineFragmentPadding
         noteTextView.textContainerInset = UIEdgeInsets(top: 0, left: -lineFramentPadding, bottom: 0, right: -lineFramentPadding)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+
+        paragraphStyle.lineSpacing = 6
+        
+        let typingAttributes: [NSAttributedString.Key: Any] = [
+            .paragraphStyle: paragraphStyle,
+            .font: UIFont.systemFont(ofSize: 14),
+            .foregroundColor: UIColor.secondaryLabel
+        ]
+        noteTextView.typingAttributes = typingAttributes
+        
+        noteTextView.tintColorDidChange()
     }
 }
